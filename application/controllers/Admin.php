cb54cecc7ea281bfb83fee46c9466d6d0d249ed6
hanifahfanidyautari <45959864+hanifahfanidyautari@users.noreply.github.com>
2019-06-25 07:53:32 +0700

    v

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,18 +6,20 @@ class Admin extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('SupplierModel');
+		$this->load->model('Purch_reqModel');
 		 $this->load->helper('url','form','download');
 		  $this->load->library('excel','upload');
 
+
 	
 	}
-	public function index()
-	
+
+public function index()
 	{
-		$data['supp']= $this->SupplierModel->getSupplier();
-		$this->load->view('admin/header');
-		$this->load->view('admin/supplier',$data);
+		$data['Purch_req']= $this->Purch_reqModel->getPurch_req();
+		$this->load->view('Admin/header');
+        $this->load->view('Admin/Purch_req',$data);
+   
 	}
 
 	public function setting(){
