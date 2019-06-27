4e42a63b40fd1bb2b7af747d74dae8dce2d90fc3
malakdiana <malakdiana00@gmail.com>
2019-06-27 11:49:10 +0700

    xx

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -7,15 +7,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/png" href="<?php echo base_url()?>assets/images/icon/favicon.ico">
     <link rel="stylesheet" href="<?php echo base_url()?>assets/css/bootstrap.min.css">
+   
+  
     <link rel="stylesheet" href="<?php echo base_url()?>assets/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php echo base_url()?>assets/css/themify-icons.css">
     <link rel="stylesheet" href="<?php echo base_url()?>assets/css/metisMenu.css">
     <link rel="stylesheet" href="<?php echo base_url()?>assets/css/owl.carousel.min.css">
     <link rel="stylesheet" href="<?php echo base_url()?>assets/css/slicknav.min.css">
-     
+    
     <!-- amchart css -->
     
     <!-- others css -->
+      <link rel="stylesheet" href="<?php echo base_url()?>assets/css/nav.css">
     <link rel="stylesheet" href="<?php echo base_url()?>assets/css/typography.css">
     <link rel="stylesheet" href="<?php echo base_url()?>assets/css/default-css.css">
     <link rel="stylesheet" href="<?php echo base_url()?>assets/css/styles.css">
--- application/views/admin/tambahVendor.php
+++ application/views/admin/tambahVendor.php
@@ -1,4 +1,4 @@
-
+ <link rel="stylesheet" href="<?php echo base_url()?>assets/css/nav.css">
  <div class="page-title-area">
                 <div class="row align-items-center">
                     <div class="col-sm-6">
@@ -20,14 +20,21 @@
                 <div class="row">
                     <!-- data table start -->
                     <div class="col-12 mt-5">
-                        <div class="card">
+                      
+                                    <div class="container"> 
+        
+
+                                    <div class="w3-bar w3-indigo">
+  <button class="w3-bar-item w3-button" onclick="openCity('London')">Tambah Vendor</button>
+  <button class="w3-bar-item w3-button" onclick="openCity('Paris')">List Vendor</button>
+</div>
+  <div class="card">
                                 <div class="card-body">
-                                            <button type="button" class="btn btn-success" id="btn-tambah-form">Tambah Data Form</button>
-        <button type="button" id="btn-reset-form" class="btn btn-primary">Reset Form</button><br><br>
-                                     <?php echo form_open_multipart('Qr/tambahVen/'.$id) ?>
-                                     <h3 align="center">Form Tambah Vendor</h3><br>
-                                       
-                                               <div style="margin-bottom: 10px"> 
+<div id="London" class="w3-container w3-display-container city">
+ <button type="button" class="btn btn-success" id="btn-tambah-form">Tambah Data Form</button> <button type="button" id="btn-reset-form" class="btn btn-primary">Reset Form</button><br><br>
+                   <?php echo form_open_multipart('Qr/tambahVen/'.$id) ?>
+                        <h3 align="center">Form Tambah Vendor</h3><br>
+                                        <div style="margin-bottom: 10px"> 
                                                    <hr>Data Vendor 1
                                                </div>
                                                <input type="hidden" class="form-control" name="id[]" id="id" value="<?php echo $id; ?>" >
@@ -48,23 +55,51 @@
                                            
                                                 <input type="file" class="form-control" name="userfiles[]" style="margin-bottom: 25px">
                                             </div>
-    <div id="insert-form"></div>
-                                           
-   
-   
-                                     
-        
-          
+                                            <div id="insert-form"></div>          
               <div align="right"> <button type="submit" class="btn btn-primary" style="align-self: right">Simpan</button></div>
               <?php echo form_close() ?>
-                <input type="hidden" id="jumlah-form" value="1">
-           
+                <input type="hidden" id="jumlah-form" value="1">    
+</div>
 
-    </div>
+<div id="Paris" class="w3-container w3-display-container city" style="display:none">
+ 
+  <table cellpadding="0" cellspacing="0" border="0" class="table table-striped" id="dataTablesss">
+                                        <thead class="bg-light text-capitalize">
+                                            <tr>
+                                                <th>NO</th>
+                                                <th>Tanggal</th>
+                                                <th>Nama Supplier</th>
+                                                <th>Harga</th>
+                                                <th>Attacment</th>
+                                                <th >ACTION</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php   $no=1; foreach ($vendor as $key) {?>
+                                            <tr>
+                                            
+                                                <td><?php echo $no;?></td>
+                                                <td><?php echo $key->tanggal;?></td>
+                                                <td><?php echo $key->nama_vendor;?></td>
+                                                <td><?php echo $key->harga;?></td>
+                                                <td><?php echo $key->detail;?></td>
+                                                <td><button>edit</button></td></tr>
+                                                <?php $no++;} ?>
+                                       
+                                        </tbody>
+                                    </table>
+                                                
+</div>
+
+
 </div>
 </div>
 </div>
 </div>
+</div>
+</div>
+
+
  <?php $this->load->view('admin/footer'); ?>
  <script>
     $(document).ready(function(){ // Ketika halaman sudah diload dan siap
@@ -88,3 +123,13 @@
         });
     });
     </script>
+   <script>
+function openCity(cityName) {
+  var i;
+  var x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById(cityName).style.display = "block";  
+}
+</script>
