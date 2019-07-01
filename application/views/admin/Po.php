e4b2508ecc0d32a3c3efdcb667f188811b20cf0f
hanifahfanidyautari <45959864+hanifahfanidyautari@users.noreply.github.com>
2019-07-01 13:14:36 +0700

    b

--- application/views/admin/Po.php
+++ application/views/admin/Po.php
@@ -56,7 +56,7 @@
                                    
                                     <button type="button" class="btn btn-info" style="width:80px; height:50px;"><a href="<?php echo site_url()?>/Po/GetItem_barang/<?php echo $key->id_po?>"><font color="white"><i class="fa fa-th-list"></i> Detail</font></a></button>
 
-                                     <button type="button" class="btn btn-success" style="width:80px; height:50px;"> <a href="<?php echo site_url()?>/Po/tambahItem/<?php echo $key->id_po?>"><font color="white"><i class="fa fa-plus"></i> Insert</font></a></button>
+                                     <button type="button" class="btn btn-success" style="width:95px; height:50px;"> <a href="<?php echo site_url()?>/Po/tambahItem/<?php echo $key->id_po?>"><font color="white"><i class="fa fa-plus"></i> Insert PR</font></a></button>
 
 
                                      <button type="button" class="btn btn-primary" style="width:80px; height:50px;"> <a href="<?php echo site_url()?>/Po/EditPo/<?php echo $key->id_po?>"><font color="white"><i class="fa fa-pencil-square-o"></i> Edit</font></a></button>
