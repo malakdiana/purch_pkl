60346ff8195d5280b8bc6fabab0f4decfad707db
malakdiana <malakdiana00@gmail.com>
2019-07-19 15:27:43 +0700

    bb

--- application/views/admin/tambahSupplier.php
+++ application/views/admin/tambahSupplier.php
@@ -54,7 +54,7 @@
                                              <label class="control-label" for="alamat">No Telepon :</label>
                                          </div>
                                          <div class="col-sm-4">
-                                            <input type="number" max="20" class="form-control" id="no_telp" name="no_telp">
+                                            <input type="text" max="20" class="form-control" id="no_telp" name="no_telp">
                                         </div>
 
                                         </div>
@@ -65,7 +65,7 @@
                                                 <label class="control-label" for="alamat">No Fax :</label>
                                             </div>
                                             <div class="col-sm-4">
-                                                <input type="number" max="50" class="form-control" id="no_fax" name="no_fax" style="margin-bottom: 25px">
+                                                <input type="text" max="50" class="form-control" id="no_fax" name="no_fax" style="margin-bottom: 25px">
                                             </div>
                                             <div class="col-md-2">
                                              <label class="control-label" for="attention">Attention :</label>
@@ -82,7 +82,7 @@
                                                 <label class="control-label" for="no_hp">No HP :</label>
                                             </div>
                                             <div class="col-sm-4">
-                                                <input type="number" max="50" class="form-control" id="no_hp" name="no_hp" style="margin-bottom: 25px">
+                                                <input type="text" max="50" class="form-control" id="no_hp" name="no_hp" style="margin-bottom: 25px">
                                             </div>
                                             <div class="col-md-2">
                                              <label class="control-label" for="tgl_input">Tanggal Input :</label>
@@ -100,7 +100,7 @@
                                                 <label class="control-label" for="nomer_rek">No Rek:</label>
                                             </div>
                                             <div class="col-sm-4">
-                                                <input type="number" max="30" class="form-control" id="nomer_rek" name="nomer_rek" style="margin-bottom: 25px">
+                                                <input type="text" class="form-control" id="nomer_rek" name="nomer_rek" style="margin-bottom: 25px">
                                             </div>
                                             <div class="col-md-2">
                                              <label class="control-label" for="bank">Bank:</label>
@@ -189,3 +189,27 @@
 </div>
 </div>
 </div>
+
+<script type="text/javascript">
+  var rupiah = document.getElementById("nomer_rek");
+rupiah.addEventListener("keyup", function(e) {
+  rupiah.value = formatRupiah(this.value,);
+});
+
+  var no_hp = document.getElementById("no_hp");
+no_hp.addEventListener("keyup", function(e) {
+  no_hp.value = formatRupiah(this.value,);
+});
+  var no_fax = document.getElementById("no_fax");
+no_fax.addEventListener("keyup", function(e) {
+  no_fax.value = formatRupiah(this.value,);
+});
+  var no_telp = document.getElementById("no_telp");
+no_telp.addEventListener("keyup", function(e) {
+  no_telp.value = formatRupiah(this.value,);
+});
+function formatRupiah(angka) {
+  var number_string = angka.replace(/[^,\d]/g, "").toString();
+  return number_string;
+  }
+</script>
