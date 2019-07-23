04ddf40387c1625d7a5036975c8ef9f611c7dda2
malakdiana <malakdiana00@gmail.com>
2019-07-23 13:29:30 +0700

    log

--- application/views/admin/tambahLogin.php
+++ application/views/admin/tambahLogin.php
@@ -46,11 +46,27 @@
                                      <div class="col-4 mt-6">
                                     <h4> <i class="fa fa-user"></i> Form Tambah User</h4> <br><br>
                                     </div>
+                                     <div class="form-group" >
+                                                     <label class="control-label " for="hak_akses">Hak Akses :</label>
+                                           
+                                                <select class="form-control" name="hak_akses" style="margin-bottom: 25px; width:400px;" required="" id="hak_akses">
+
+                                                    <option class="form-control" value="">Pilih: </option>
+                                                     <option class="form-control" value="1">ADMINISTRATOR</option>
+                                                     <option class="form-control" value="2">SECTION</option>
+                                                     <option class="form-control" value="3">INVOICE</option>
+                                                     <option class="form-control" value="4">USER PERSONAL</option>
+
+                                                </select>
+                                            </div>
+                                            <div class="form-group" id="insert-form" name="insert-form">
+                                                
+                                            </div>
                                        
                                      <div class="form-group" >
                                                      <label class="control-label " for="username">Username:</label>
                                            
-                                                <input type="text" class="form-control" name="username" style="margin-bottom: 25px; width:400px; " autocomplete="off" required="">
+                                                <input type="text" class="form-control" name="username" id="username" style="margin-bottom: 25px; width:400px; " autocomplete="off" required="">
                                             </div>  
 
                                             <div class="form-group" >
@@ -78,22 +94,7 @@
                                                 
                                             </div>
 
-                                            <div class="form-group" >
-                                                     <label class="control-label " for="hak_akses">Hak Akses :</label>
                                            
-                                                <select class="form-control" name="hak_akses" style="margin-bottom: 25px; width:400px;" required="" id="hak_akses">
-
-                                                    <option class="form-control" value="">Pilih: </option>
-                                                     <option class="form-control" value="1">ADMINISTRATOR</option>
-                                                     <option class="form-control" value="2">SECTION</option>
-                                                     <option class="form-control" value="3">INVOICE</option>
-                                                     <option class="form-control" value="4">USER PERSONAL</option>
-
-                                                </select>
-                                            </div>
-                                            <div class="form-group" id="insert-form" name="insert-form">
-                                                
-                                            </div>
 
                                            
                                           
@@ -113,25 +114,42 @@
 </div>
 </div>
  <?php $this->load->view('admin/footer'); ?>
+ <div id="scriptt"></script></div>
 
 <script>
     $(document).ready(function(){ // Ketika halaman sudah diload dan siap
         $("#hak_akses").click(function(){ // Ketika tombol Tambah Data Form di klik
              // Ambil jumlah data form pada textbox 
              var val = $(this).val();
-             if (val == "4") {
-
+             console.log(val);
+             if (val == "4" ) {
+$("#insert-form").html("");
+$("#scriptt").html("");
               $("#insert-form").append(" <div class='form-group' ><label class='control-label ' for='hak_akses'>Section :</label><select name='section' class='form-control' id='section' required style='margin-bottom: 25px; width:400px;'><option value=''> PILIH : </option><?php foreach ($section as $key) {?> <option class='form-control' value='<?php echo $key->id_section?>'><?php echo $key->nama_section?> </option> <?php }?></select></div>");
+              document.getElementById('username').disabled= false;
+             
 
-          }else{
-             $("#insert-form").html("");
+          }else if( val== "2"){
+            $("#insert-form").html("");
+            $("#scriptt").html("");
+            document.getElementById('username').value= "";
+              $("#insert-form").append(" <div class='form-group' ><label class='control-label ' for='hak_akses'>Section :</label><select name='section' class='form-control' id='section' required style='margin-bottom: 25px; width:400px;'><option value=''> PILIH : </option><?php foreach ($section as $key) {?> <option class='form-control' value='<?php echo $key->nama_section?>'><?php echo $key->nama_section?> </option> <?php }?></select></div>");
+              $("#scriptt").append("<script>$('#section').click(function(){var vall = $(this).val();var hak_akses = $('#hak_akses').val();    if(hak_akses == '2'){ document.getElementById('username').value = vall;document.getElementById('username').disabled= true;}    });");
           }
 
-             // Ubah value textbox jumlah-form dengan variabel nextform
+          else{
+             $("#insert-form").html("");
+              $("#scriptt").html("");
+             document.getElementById('username').disabled= false;
+          }
+
+          
         });   
-        // Buat fungsi untuk mereset form ke semula
+     
        
     });
+
+  
 
 
 $("#passwordfield").on("keyup",function(){
@@ -147,7 +165,6 @@ $(".fa-eye").mousedown(function(){
             }).mouseout(function(){
                 $("#passwordfield").attr('type','password');
             });
-
 
 
 $("#konfirpasswordfield").on("keyup",function(){
