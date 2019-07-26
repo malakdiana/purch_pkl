47c68363dee58230d13997479b072dea46dd8958
hanifahfanidyautari <45959864+hanifahfanidyautari@users.noreply.github.com>
2019-07-26 07:44:41 +0700

    v

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -12,6 +12,9 @@ class Login extends CI_Controller {
          $this->load->helper('url','form');
         // $this->load->library(array('PHPExcel','PHPExcel/IOFactory'));
          // $this->load->library('excel','upload');
+         if (!$this->session->userdata('logged_in')) {
+	      redirect('Login','refresh');
+	  }
 
     
     }
